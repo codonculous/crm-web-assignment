9c42b4a0899e7e9f872e38590bc1125a87e328c9
codonculous <feidameng01@gmail.com>
2017-04-06 00:53:28 -0400

    add edit and delete function to crm

--- crm.rb
+++ crm.rb
@@ -5,20 +5,15 @@
 require 'sinatra'
 require_relative 'contact'
 
-# Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-# Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-# Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+
 
 get '/' do
-  @crm_app_name = "Bitmaker's CRM"
-  @current_time = Time.now
-  erb :index
-end
-
-get '/contacts' do
 
   @contacts_num = Contact.all.length
-  erb :contacts
+  erb :index
 
 end
 
@@ -27,8 +22,49 @@ get '/contacts/new' do
   erb :new_contact
 end
 
-post '/contacts' do
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+post '/' do
 
   Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
-  redirect to('/contacts')
+  redirect to('/')
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+    redirect to('/')
+  else
+    raise Sinatra::NotFound
+  end
 end
